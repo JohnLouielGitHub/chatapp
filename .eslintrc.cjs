463d62d9c6aed4f854b9f0cc3c5c00489ad57c58
CodeLou <tamargo.louiel@gmail.com>
2023-04-25 00:26:16 +0800

    installed tailwind and eslint + prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+    ],
+    overrides: [],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    plugins: ['react', '@typescript-eslint', 'tailwindcss', 'prettier'],
+    rules: {
+        'tailwindcss/classnames-order': 'warn',
+        'tailwindcss/no-custom-classname': 'warn',
+        'tailwindcss/no-contradicting-classname': 'error',
+        'react/react-in-jsx-scope': 0,
+    },
+}
