19033e7658e64fff10db8c83749afd3878d170d6
CodeLou <tamargo.louiel@gmail.com>
2023-04-25 23:17:50 +0800

    setup prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,6 @@ module.exports = {
         es2021: true,
     },
     extends: [
-        'eslint:recommended',
         'plugin:react/recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:prettier/recommended',
@@ -21,4 +20,4 @@ module.exports = {
         'tailwindcss/no-contradicting-classname': 'error',
         'react/react-in-jsx-scope': 0,
     },
-}
+};
